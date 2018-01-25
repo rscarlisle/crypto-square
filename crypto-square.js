3ea16b56402f5494ff22331d011a96453feafffa
user <user@users-MacBook-Pro.local>
2018-01-25 15:47:04 -0800

    added comments to clarify Crypto function

--- crypto-square.js
+++ crypto-square.js
@@ -1,7 +1,10 @@
+// for spec to consume the Crypto object, added module.exports
 const Crypto = function (rawString) {
     this.rawString = rawString;
 };
 
+// used prototype to add normalizePlaintext() to Crypto()
+// use of 'this' refers to local parent container
 Crypto.prototype.normalizePlaintext = function () {
     const pattern = /[^\w0-9]/gi
     return this.rawString.replace(pattern, '').toLowerCase()
