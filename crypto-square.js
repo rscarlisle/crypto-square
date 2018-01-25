dea6fb62e4dd3e455766d6082ac5fcf64f0193f6
user <user@users-MacBook-Pro.local>
2018-01-25 15:49:37 -0800

    removed old, commented out code

--- crypto-square.js
+++ crypto-square.js
@@ -10,13 +10,4 @@ Crypto.prototype.normalizePlaintext = function () {
     return this.rawString.replace(pattern, '').toLowerCase()
 };
 
-
-
-// var normalizePlaintext = function (rawString) {
-//     var pattern = /[^\w0-9]/gi;
-//     return rawString.replace(pattern, '').toLowerCase()
-// };
-
-// var result = normalizePlaintext('s#$%^&plunk');
-
 module.exports = Crypto;
